32fa0d4fce5424175ea554263ead84b652a9bbf0
Carlos081023 <r.carlos90235@gmail.com>
2024-11-09 22:37:50 -0600

    Add files via upload

--- Portfolio Project 2 - Inventory Analysis Code.sql
+++ Portfolio Project 2 - Inventory Analysis Code.sql
@@ -131,7 +131,7 @@ ORDER BY total_sales DESC;
 
 
 -- Top 3 products by category and sales for each year
-WITH top_3 AS(
+WITH top_product_insights AS(
 	SELECT
 		YEAR(ord.order_date) as year,
 		prod.category AS category,
@@ -145,23 +145,24 @@ WITH top_3 AS(
 	GROUP BY YEAR(ord.order_date), prod.category, prod.product_id
 )
 SELECT * 
-FROM top_3
+FROM top_product_insights
 WHERE product_rank < 4;
 
 -- Bottom 3 Products of each year by sales
 WITH product_insights AS(
-	SELECT 
+	SELECT
 		YEAR(ord.order_date) as year,
-		prod.product_name AS product,
+		prod.category AS category,
+		prod.product_id AS product_id,
 		SUM(ord.sales) AS total_sales,
-		DENSE_RANK() OVER(PARTITION BY YEAR(ord.order_date) ORDER BY SUM(ord.sales) ASC) AS product_rank
+		DENSE_RANK() OVER(PARTITION BY YEAR(ord.order_date),prod.category ORDER BY SUM(ord.sales) ASC) AS product_rank
 	FROM products AS prod
 	LEFT JOIN orders AS ord
-		ON prod.product_id=ord.product_id
+		ON prod.Product_ID=ord.Product_ID
 	WHERE YEAR(ord.order_date) IS NOT NULL
-	GROUP BY YEAR(ord.order_date), prod.product_name
+	GROUP BY YEAR(ord.order_date), prod.category, prod.product_id
 )
-SELECT year, product,product_rank
+SELECT *
 FROM product_insights
 WHERE product_rank < 4;
 
@@ -219,3 +220,146 @@ SELECT
 FROM orders
 GROUP BY YEAR(order_date), MONTH(order_date)
 ORDER BY YEAR(order_date), MONTH(order_date);
+
+-- ABC Analysis Discontinuing Product Recommendations
+
+-- Lets gather aggregates of Profits, Sales, and Total Cost
+SELECT
+	SUM(Profit),
+    SUM(Sales),
+    SUM(profit-sales) AS cogs
+FROM orders;
+
+-- Now lets examine removing products with sales figures below 100 and see the impact
+WITH discontinued_products_whatif AS(
+	SELECT
+		product_id,
+		SUM(sales) AS total_sales,
+		SUM(profit) AS total_profit,
+		SUM(profit-sales) AS total_cogs
+	FROM orders
+	GROUP BY product_id
+	HAVING SUM(sales) < 100
+	ORDER BY SUM(sales) DESC
+)
+SELECT 
+	SUM(total_profit),
+	SUM(total_sales),
+    SUM(total_cogs),
+    ROUND(SUM(total_profit) / (SELECT SUM(profit) FROM orders),2) * 100 AS profit_total_percentage,
+    ROUND(SUM(total_cogs) / (SELECT SUM(profit-sales) FROM orders),2) * 100 AS cogs_total_percentage,
+    ROUND(SUM(total_sales) / (SELECT SUM(sales) FROM orders),2) * 100 AS sales_total_percentage
+FROM discontinued_products_whatif;
+
+-- If we were to discontinue all products below 100 in sales, a 2% reduction in profit, 1% reduction in sales and total cogs.
+WITH discontinued_products_whatif AS(
+	SELECT
+		product_id,
+		SUM(sales) AS total_sales,
+		SUM(profit) AS total_profit,
+		SUM(profit-sales) AS total_cogs
+	FROM orders
+	GROUP BY product_id
+	HAVING SUM(sales) < 100
+	ORDER BY SUM(sales) DESC
+), products_over_avg AS(
+	SELECT *,
+		(total_cogs/total_sales) AS cost_percentage,
+		AVG(total_cogs/total_sales) OVER() AS average_cost_percentage
+	FROM discontinued_products_whatif
+), discontinue_products_whatif2 AS(
+	SELECT *
+	FROM products_over_avg
+	WHERE ABS(cost_percentage) > ABS(average_cost_percentage)
+) 
+SELECT 
+	SUM(total_sales),
+    SUM(total_profit),
+    SUM(total_cogs),
+    ROUND(SUM(total_profit) / (SELECT SUM(profit) FROM orders),3) * 100 AS profit_total_percentage,
+    ROUND(SUM(total_cogs) / (SELECT SUM(profit-sales) FROM orders),3) * 100 AS cogs_total_percentage,
+    ROUND(SUM(total_sales) / (SELECT SUM(sales) FROM orders),3) * 100 AS sales_total_percentage
+FROM discontinue_products_whatif2;
+
+-- If we were to remove products whose sales were less than 100 AND whose cost percentage is greater than the AVG cost percentage, profit would reduce by .1%, cost would reduce by .5, and sales would reduce by .4
+
+-- Now Im going to switch the requirements to 350 in sales
+WITH discontinued_products_under350 AS(
+	SELECT
+		product_id,
+		SUM(sales) AS total_sales,
+		SUM(profit) AS total_profit,
+		SUM(profit-sales) AS total_cogs
+	FROM orders
+	GROUP BY product_id
+	HAVING SUM(sales) < 350
+	ORDER BY SUM(sales) DESC
+)
+SELECT 
+	SUM(total_profit),
+	SUM(total_sales),
+    SUM(total_cogs),
+    ROUND(SUM(total_profit) / (SELECT SUM(profit) FROM orders),2) * 100 AS profit_total_percentage,
+    ROUND(SUM(total_cogs) / (SELECT SUM(profit-sales) FROM orders),2) * 100 AS cogs_total_percentage,
+    ROUND(SUM(total_sales) / (SELECT SUM(sales) FROM orders),2) * 100 AS sales_total_percentage
+FROM discontinued_products_under350;
+
+-- If we were to remove all products below 350 in sales, a 10% reduction in profit would occur, 5% reduction in total cost and sales would occur
+
+-- Now we will see if products above the average cost percentage are removed
+WITH discontinued_products_under350 AS(
+	SELECT
+		product_id,
+		SUM(sales) AS total_sales,
+		SUM(profit) AS total_profit,
+		SUM(profit-sales) AS total_cogs
+	FROM orders
+	GROUP BY product_id
+	HAVING SUM(sales) < 350
+	ORDER BY SUM(sales) DESC
+), products_over_avg_2 AS(
+	SELECT *,
+		(total_cogs/total_sales) AS cost_percentage,
+		AVG(total_cogs/total_sales) OVER() AS average_cost_percentage
+	FROM discontinued_products_under350
+), discontinue_products_over_costper AS(
+	SELECT *
+	FROM products_over_avg_2
+	WHERE ABS(cost_percentage) > ABS(average_cost_percentage)
+) 
+SELECT 
+	SUM(total_profit) AS total_profit,
+	SUM(total_sales) AS total_sales,
+    SUM(total_cogs) AS total_cost,
+    ROUND(SUM(total_profit) / (SELECT SUM(profit) FROM orders),3) * 100 AS profit_total_percentage,
+    ROUND(SUM(total_cogs) / (SELECT SUM(profit-sales) FROM orders),3) * 100 AS cogs_total_percentage,
+    ROUND(SUM(total_sales) / (SELECT SUM(sales) FROM orders),3) * 100 AS sales_total_percentage,
+    COUNT(Product_id) AS removed_products
+FROM discontinue_products_over_costper;
+
+-- Removing these products would account for a .9% reduction in profit, a 2.6% reduction in total cost, and a reduction of 2.4 in sales. This would lead to overall better cost effectiveness and improve
+-- inventory holding costs associated with these products.
+
+WITH discontinued_products_under350 AS(
+	SELECT
+		product_id,
+		SUM(sales) AS total_sales,
+		SUM(profit) AS total_profit,
+		SUM(profit-sales) AS total_cogs
+	FROM orders
+	GROUP BY product_id
+	HAVING SUM(sales) < 350
+		AND SUM(profit) < 0
+	ORDER BY SUM(sales) DESC
+)
+SELECT 
+	SUM(total_sales),
+    SUM(total_profit),
+    SUM(total_cogs),
+	ROUND(SUM(total_profit) / (SELECT SUM(profit) FROM orders),3) * 100 AS profit_total_percentage,
+    ROUND(SUM(total_cogs) / (SELECT SUM(profit-sales) FROM orders),3) * 100 AS cogs_total_percentage,
+    ROUND(SUM(total_sales) / (SELECT SUM(sales) FROM orders),3) * 100 AS sales_total_percentage,
+    COUNT(Product_id) AS removed_products
+FROM discontinued_products_under350;
+
+-- If we remove products that make less than 350 in total sales and products with negative profits, we will see a total of 3575 dollars in negative profit removed, a .9% reduction in total cost and a .6% reduction in sales.
